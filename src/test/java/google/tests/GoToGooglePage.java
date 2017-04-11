295ef68f8111cce676b517d697136e0cda4a152f
azzmiks <myroslava_zubach@epam.com>
2017-04-11 22:46:07 +0200

    GoToGooglePage class

---
+++ src/test/java/google/tests/GoToGooglePage.java
@@ -0,0 +1,37 @@
+package google.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Test;
+/**
+ * Created by azmiks on 11/04/2017.
+ */
+public class GoToGooglePage {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+
+        driver = new ChromeDriver();
+    }
+
+    // @BeforeMethod - what can we use in Junit instead of it
+    //   public void openHomePage() {
+    //driver.get("http://www.google.com");
+    //}
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void test(){
+
+        driver.get("http://www.google.com");
+    }
+
+}
